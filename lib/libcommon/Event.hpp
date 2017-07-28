ee2917936cf16896982ec3d7173806e9892a2348
Chris Price <chrisprice@rogers.com>
2017-07-28 18:20:28 -0400

    libcommon: Event class
    This is a simple class that can be handed out to a consumer
    in order to notify them when an event occurs.

---
+++ lib/libcommon/Event.hpp
@@ -0,0 +1,31 @@
+namespace common
+{
+
+class Event
+{
+public:
+  Event()
+    : m_signalled(false)
+  {}
+
+  void signal()
+  {
+    m_signalled = true;
+    m_cv.notify_one();
+  }
+
+  void wait()
+  {
+    std::unique_lock<std::mutex> lock(m_mtx);
+    m_cv.wait(lock, [this] () {
+      return m_signalled;
+    });
+  }
+
+private:
+  std::mutex m_mtx;
+  std::condition_variable m_cv;
+  bool m_signalled;
+};
+
+}; // namespace common
