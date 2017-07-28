32a7027c88bcd13ca3731733a4ed89fb678ae117
Chris Price <chrisprice@rogers.com>
2017-07-28 19:25:12 -0400

    libcommon: Make futures cancellable

--- src/lib/libcommon/Future.hpp
+++ src/lib/libcommon/Future.hpp
@@ -9,21 +9,33 @@ class Future
 public:
   Future() {}
 
-  void set(T value)
+  bool set(T value)
   {
     m_value = value;
     m_event.signal();
   }
 
-  T value()
+  bool cancel()
+  {
+    m_cancelled = true;
+    m_event.signal();
+  }
+
+  bool wait()
   {
     m_event.wait();
+    return !m_cancelled;
+  }
+
+  T value()
+  {
     return m_value;
   }
 
 private:
   Event m_event;
   T m_value;
+  bool m_cancelled;
 };
 
 }; // namespace common
