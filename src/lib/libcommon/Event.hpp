6951aacf8f4b6e6c0d14691e53cb8aecd242c256
Chris Price <chrisprice@rogers.com>
2017-07-29 09:54:20 -0400

    libcommon: Improvements to Future and Event
    Eliminate race conditions, add a few more public methods,
    and include some documentation. Also fixes up the indentation
    scheme.

--- src/lib/libcommon/Event.hpp
+++ src/lib/libcommon/Event.hpp
@@ -7,28 +7,55 @@ namespace common
 class Event
 {
 public:
-  Event()
-    : m_signalled(false)
-  {}
+    Event()
+        : m_signalled(false)
+    {}
 
-  void signal()
-  {
-    m_signalled = true;
-    m_cv.notify_one();
-  }
+    /**
+     * Returns true if the event has been signalled
+     * and false otherwise. Note that wait() will not
+     * block if and only if this function returns true.
+     */
+    bool signalled()
+    {
+        return m_signalled;
+    }
 
-  void wait()
-  {
-    std::unique_lock<std::mutex> lock(m_mtx);
-    m_cv.wait(lock, [this] () {
-      return m_signalled;
-    });
-  }
+    /**
+     * Signal the event. This function is meant to
+     * be called by the producer of the event. Returns
+     * true if the event was signalled, and false if it
+     * has already been signalled.
+     */
+    bool signal()
+    {
+        bool signalled;
+        {
+            std::lock_guard<std::mutex> lock(m_mtx);
+            signalled = this->signalled();
+            m_signalled = true;
+        }
+        m_cv.notify_all();
+        return !signalled;
+    }
+
+    /**
+     * Wait for the event to occur. This function
+     * is meant to be used by the consumer of the
+     * event and will block until the event occurs.
+     */
+    void wait()
+    {
+        std::unique_lock<std::mutex> lock(m_mtx);
+        m_cv.wait(lock, [this] () {
+                return signalled();
+            });
+    }
 
 private:
-  std::mutex m_mtx;
-  std::condition_variable m_cv;
-  bool m_signalled;
+    std::mutex m_mtx;
+    std::condition_variable m_cv;
+    bool m_signalled;
 };
 
 }; // namespace common
--- src/lib/libcommon/Future.hpp
+++ src/lib/libcommon/Future.hpp
@@ -1,3 +1,5 @@
+#include <mutex>
+
 #include "Event.hpp"
 
 namespace common
@@ -7,37 +9,94 @@ template <typename T>
 class Future
 {
 public:
-  Future() {}
+    Future() {}
 
-  bool set(T value)
-  {
-    m_value = value;
-    m_event.signal();
-    return true;
-  }
+    /**
+     * Set the value of the future. This function
+     * is intended to be called by the producer
+     * when computation of the value is complete.
+     * Returns true if the value of the future was
+     * successfully set and false otherwise.
+     *
+     * NOTE: the value of a future may only be set
+     *       once, so subsequent calls to this function
+     *       will return false.
+     */
+    bool set(T value)
+    {
+        std::lock_guard<std::mutex> lock(m_mtx);
+        bool signalled = m_event.signalled();
+        if (!signalled)
+        {
+            m_value = value;
+            m_event.signal();
+        }
+        return !signalled;
+    }
 
-  bool cancel()
-  {
-    m_cancelled = true;
-    m_event.signal();
-    return m_cancelled;
-  }
+    /**
+     * Cancel the future. This function is meant to
+     * be called by a producer if the value cannot or
+     * will not be computed. Note that this function
+     * is idempotent. Returns true if this call or a
+     * previous call has cancelled the future, and false
+     * otherwise.
+     *
+     * NOTE: false will be returned in the case that
+     *       the future value has already been set
+     *       by a call to set().
+     */
+    bool cancel()
+    {
+        std::lock_guard<std::mutex> lock(m_mtx);
+        if (!m_event.signalled())
+        {
+            m_cancelled = true;
+            m_event.signal();
+        }
+        return m_cancelled;
+    }
 
-  bool wait()
-  {
-    m_event.wait();
-    return !m_cancelled;
-  }
+    /**
+     * Determine if a call to wait() will block. Returns
+     * true if the call will return immediately and
+     * false otherwise.
+     */
+    bool tryWait()
+    {
+        return m_event.signalled();
+    }
 
-  T value()
-  {
-    return m_value;
-  }
+    /**
+     * Wait for the future to be computed. This function
+     * will block until either the future value is set
+     * or until the future is cancelled. Returns true if
+     * the future value has been computed and false if
+     * it has been cancelled.
+     */
+    bool wait()
+    {
+        m_event.wait();
+        return !m_cancelled;
+    }
+
+    /**
+     * Get the future value. Note that, before wait()
+     * returns or if wait() returns false, then the
+     * value will be a default-constructed T. After
+     * wait() returns true, value() returns the
+     * future value.
+     */
+    T value()
+    {
+        return m_value;
+    }
 
 private:
-  Event m_event;
-  T m_value;
-  bool m_cancelled;
+    std::mutex m_mtx;
+    Event m_event;
+    T m_value;
+    bool m_cancelled;
 };
 
 }; // namespace common
