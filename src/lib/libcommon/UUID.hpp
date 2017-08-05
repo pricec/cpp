fe99cf81e48f055aab20593836c3191cd169618c
Chris Price <chrisprice@rogers.com>
2017-08-04 21:30:20 -0400

    libcommon: UUID class

---
+++ src/lib/libcommon/UUID.hpp
@@ -0,0 +1,59 @@
+#pragma once
+#include <random>
+
+namespace common
+{
+
+class UUID
+{
+public:
+    UUID()
+        : m_major()
+        , m_minor()
+    {
+        std::mt19937 rng;
+        rng.seed(std::random_device()());
+        std::uniform_int_distribution<
+            std::mt19937::result_type
+        > d(0, UINT64_MAX);
+
+        m_major = d(rng);
+        m_minor = d(rng);
+    }
+
+    UUID(const UUID &other)
+        : m_major(other.m_major)
+        , m_minor(other.m_minor)
+    {}
+
+    UUID(uint64_t major, uint64_t minor)
+        : m_major(major)
+        , m_minor(minor)
+    {}
+
+    UUID& operator=(const UUID &rhs)
+    {
+        m_major = rhs.m_major;
+        m_minor = rhs.m_minor;
+        return *this;
+    }
+
+    bool operator<(const UUID &rhs)
+    {
+        if (m_major < rhs.m_major)
+        {
+            return true;
+        }
+        else if (m_major > rhs.m_major)
+        {
+            return false;
+        }
+        return m_minor < rhs.m_minor;
+    }
+
+private:
+    uint64_t m_major;
+    uint64_t m_minor;
+};
+
+}; // namespace common
