7fcd58dd1eb6eb4308182b869e98825a5c4b5b45
Chris Price <chrisprice@rogers.com>
2018-01-31 18:44:15 -0500

    libcrypto: Minor improvements to hashing code

--- src/lib/libcrypto/Hash.hpp
+++ src/lib/libcrypto/Hash.hpp
@@ -4,18 +4,41 @@
 namespace crypto
 {
 
-static const char *MD5 = "$1$";
-static const char *SHA256 = "$5$";
-static const char *SHA512 = "$6$";
+enum HashFunction
+{
+    MD5 = 0,
+    SHA256,
+    SHA512,
+};
+
+static const char *MD5_STR    = "$1$";
+static const char *SHA256_STR = "$5$";
+static const char *SHA512_STR = "$6$";
+static const char *NOFUNC_STR = "NONE";
+
+const char* HashFunctionToCStr(const HashFunction hashfn)
+{
+    switch (hashfn)
+    {
+    case MD5:
+        return MD5_STR;
+    case SHA256:
+        return SHA256_STR;
+    case SHA512:
+        return SHA512_STR;
+    default:
+        return NOFUNC_STR;
+    }
+}
 
 // Compute the UNIX password hash
 int ComputePasswordHash(
-    const char        *type,
-    const std::string &input,
-    const std::string &salt,
-    std::string       &hash
+    const HashFunction type,
+    const std::string  &input,
+    const std::string  &salt,
+    std::string        &hash
 ) {
-    std::string command(type);
+    std::string command(HashFunctionToCStr(type));
     command += salt;
     char *result = crypt(input.c_str(), command.c_str());
     if (result == NULL)
